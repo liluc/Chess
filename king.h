a1a5fb6359d41b58150687f663aadca6d2f84f91
Godparticle27 <b396zhan@uwaterloo.ca>
2022-03-28 19:15:06 -0400

    Suggestions for Piece and King
    In my implementation I need to use these 2 methods

--- king.h
+++ king.h
@@ -4,12 +4,18 @@
 #include "piece.h"
 
 class Cell;
-class King : public Piece{
-    public:
+class King : public Piece
+{
+public:
     King(Cell *, int);
     std::vector<std::vector<char>> possibleMoves() override;
     void move(std::vector<char>) override;
     ~King();
+
+    /* suggestions
+    bool ischecked() const;
+    bool ischeckmate() const;
+    */
 };
 
 #endif
--- piece.h
+++ piece.h
@@ -3,14 +3,22 @@
 #include <vector>
 
 class Cell;
-class Piece{
+class Piece
+{
     Cell *cell;
     int player;
-    public:
+    /* suggestions
+    string type;    note: type for Knight is "night" for convenience
+    */
+
+public:
     Piece(Cell *, int);
     virtual std::vector<std::vector<char>> possibleMoves() = 0;
     virtual void move(std::vector<char>) = 0;
     virtual ~Piece();
+    /* suggestions
+    string getType() const;
+    */
 };
 
 #endif
