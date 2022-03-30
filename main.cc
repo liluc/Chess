f0fcae86a2e16b36bbfeff987bbaef0279f5cccb
Godparticle27 <b396zhan@uwaterloo.ca>
2022-03-29 21:12:36 -0400

    Tweaks to main

--- main.cc
+++ main.cc
@@ -5,83 +5,105 @@
 #include <iostream>
 using namespace std;
 
-//runGame()
-//setUp()
-//display()
-//undo()
-//setpiece(string)
-//setTurns(int)
-//setWinner(int)
-//concludeScore()
+// runGame()
+// setUp()
+// display()
+// undo()
+// setpiece(string)
+// setTurns(int)
+// setWinner(int)
+// concludeScore()
 
-void displaySore(Game *game){
-    cout << "Final Score:" << endl;
-    int whiteScore = game->getPalyer()[0]->getScore();
-    int blackScore = game->getPalyer()[1]->getScore();
+// I wrote a displayScore in Game so we don't have to do it here
+
+void displayScore(Game *game)
+{
+    if (game->getWinner() != 0)
+        return;
+    // cout << "Final Score:" << endl;
+    int whiteScore = game->getPlayers()[0]->getscore();
+    int blackScore = game->getPlayers()[1]->getscore();
     cout << "White Score: " << whiteScore << endl;
     cout << "Black Score: " << blackScore << endl;
 }
 
-int main(){
+int main()
+{
     string command;
     Game *game = new Game;
-    while (cin >> command){
-        if (command == "game"){ 
+    while (cin >> command)
+    {
+        if (command == "game")
+        {
             string white;
             string black;
             cin >> white >> black; // haven't handle the computer AI mode situation
-            game.runGame();
-            game.display();
+            game.runGame(white, black);
+            cout << game;
         }
 
-        else if (command == "resign"){
+        else if (command == "resign")
+        {
             int turn = game->getBoard()->getTurn() % 2;
-            setWinner (turn + 1);
+            setWinner(turn + 1);
+            game.movePiece(); // to invoke result of resignation
+            displayScore(game);
         }
 
-        else if (command == "move"){
+        else if (command == "move")
+        {
             string start;
             string end;
             cin >> start >> endl;
-            vector<char> vStart {start[0], start[1]};
-            vector<char> vEnd {end[0], end[1]};
+            vector<char> vStart{start[0], start[1]};
+            vector<char> vEnd{end[0], end[1]};
             game.movePiece(vStart, vEnd);
-            game.display();
+            cout << game;
+            displayScore(game);
         }
 
-        else if (command == "setup"){
+        else if (command == "setup")
+        {
             string type;
             cin >> type;
-            while (type != "done"){
-                if (type == "+"){
+            while (type != "done")
+            {
+                if (type == "+")
+                {
                     string piece;
                     string pos;
                     cin >> piece >> pos;
-                    vector<char> vPos {pos[0], pos[1]};
+                    vector<char> vPos{pos[0], pos[1]};
                     game.setPiece(piece, vPos);
                 }
 
-                else if (type == "-"){
+                else if (type == "-")
+                {
                     string pos;
                     cin >> pos;
-                    vector<char> vPos {pos[0], pos[1]};
+                    vector<char> vPos{pos[0], pos[1]};
                     game.setPiece("", vPos);
                 }
 
-                //colour should only be black and white for now.
-                else if (type == "="){
+                // colour should only be black and white for now.
+                else if (type == "=")
+                {
                     string player;
                     cin >> player;
-                    if (player == "white"){
+                    if (player == "white")
+                    {
+                        game.setTurns(0);
+                    }
+                    else if (player == "black")
+                    {
                         game.setTurns(1);
-                    } else if (player == "black"){
-                        game.setTurns(2);
                     }
                 }
-                
+                cout << game;
             }
         }
     }
-    displaySore(game);
+    cout << "Final Score: " << endl;
+    game.displaySore();
     delete game;
 }
